96675402a03ce4a6086bb48c0f40525cc43f2760
Joe Keenan <joe@flyingdiver.com>
2020-08-04 22:02:26 -0400

    Device Factory
    Added probe channel devices using Device Factory

--- Fireboard.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Fireboard.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -38,6 +38,7 @@ class Plugin(indigo.PluginBase):
         self.device_info = {}
 
         self.fbDevices = {}
+        self.fbChannels = {}
         self.triggers = { }
         self.knownDevices = {}
         
@@ -47,10 +48,6 @@ class Plugin(indigo.PluginBase):
 
         if not self.fbLogin(username=self.pluginPrefs['FireBoardLogin'], password=self.pluginPrefs['FireBoardPassword']):
             indigo.server.error(u"FireBoard Login Failure")
-
-    @property
-    def account_id(self):
-        return self.account_info["Account"]["Id"]
 
     def shutdown(self):
         indigo.server.log(u"Shutting down FireBoard")
@@ -85,33 +82,49 @@ class Plugin(indigo.PluginBase):
         else:
             self.logger.error(u"{}: deviceStartComm: Unknown device version: {}".format(device.name, instanceVers))
         
-        self.logger.debug("{}: deviceStartComm: Adding device ({}) to FireBoard device list".format(device.name, device.id))
-        assert device.address not in self.fbDevices
-        self.fbDevices[device.address] = device.id
-        self.needsUpdate = True
+        if device.deviceTypeId == 'fireboard':
+            self.logger.debug(u"{}: deviceStartComm: Adding device ({}) to FireBoard list".format(device.name, device.id))
+            assert device.address not in self.fbDevices
+            self.fbDevices[device.address] = device.id
+            self.logger.threaddebug(u"fbDevices = {}".format(self.fbDevices))
+            self.needsUpdate = True
+        elif device.deviceTypeId == 'fireboardChannel':
+            self.logger.debug(u"{}: deviceStartComm: Adding device ({}) to Channel list".format(device.name, device.id))
+            assert device.address not in self.fbChannels
+            self.fbChannels[device.address] = device.id
+            self.logger.threaddebug(u"fbChannels = {}".format(self.fbChannels))
+            self.needsUpdate = True
+            
         
     def deviceStopComm(self, device):
-        self.logger.debug("{}: deviceStopComm: Removing device ({}) from FireBoard device list".format(device.name, device.id))
-        assert device.address in self.fbDevices
-        del self.fbDevices[device.address]
 
-    def validateDeviceConfigUi(self, valuesDict, typeId, devId):
-        return (True, valuesDict)
+        if device.deviceTypeId == 'fireboard':
+            self.logger.debug(u"{}: deviceStopComm: Removing device ({}) from FireBoard list".format(device.name, device.id))
+            assert device.address in self.fbDevices
+            del self.fbDevices[device.address]
+        elif device.deviceTypeId == 'fireboardChannel':
+            self.logger.debug(u"{}: deviceStopComm: Removing device ({}) from Channel list".format(device.name, device.id))
+            assert device.address in self.fbChannels
+            del self.fbChannels[device.address]
 
+
+    ########################################
+    # Event Methods
+    ########################################
 
     def triggerStartProcessing(self, trigger):
-        self.logger.debug("Adding Trigger {} ({}) - {}".format(trigger.name, trigger.id, trigger.pluginTypeId))
+        self.logger.debug(u"Adding Trigger {} ({}) - {}".format(trigger.name, trigger.id, trigger.pluginTypeId))
         assert trigger.id not in self.triggers
         self.triggers[trigger.id] = trigger
 
     def triggerStopProcessing(self, trigger):
-        self.logger.debug("Removing Trigger {} ({})".format(trigger.name, trigger.id))
+        self.logger.debug(u"Removing Trigger {} ({})".format(trigger.name, trigger.id))
         assert trigger.id in self.triggers
         del self.triggers[trigger.id]
 
     def triggerCheck(self, device):
         for triggerId, trigger in sorted(self.triggers.iteritems()):
-            self.logger.debug("Checking Trigger {} ({}), Type: {}".format(trigger.name, trigger.id, trigger.pluginTypeId))
+            self.logger.debug(u"Checking Trigger {} ({}), Type: {}".format(trigger.name, trigger.id, trigger.pluginTypeId))
 
 #                indigo.trigger.execute(trigger) 
 
@@ -121,7 +134,7 @@ class Plugin(indigo.PluginBase):
     ########################################
 
     def menuDumpData(self):
-        self.logger.info("menuDumpData Devices:\n{}".format(json.dumps(self.device_info, indent=4, separators=(',', ': '))))
+        self.logger.info(u"menuDumpData Devices:\n{}".format(json.dumps(self.device_info, indent=4, separators=(',', ': '))))
         return True
         
 
@@ -195,9 +208,53 @@ class Plugin(indigo.PluginBase):
             except:
                 pass
 
-        self.logger.debug("availableDeviceList: retList = {}".format(retList))
+        self.logger.debug(u"availableDeviceList: retList = {}".format(retList))
         return retList
 
+
+    ########################################
+    # DeviceFactory methods (specified in Devices.xml):
+    ####################
+
+    def getDeviceFactoryUiValues(self, devIdList):
+        self.logger.debug(u"getDeviceFactoryUiValues: devIdList = {}".format(devIdList))
+        valuesDict = indigo.Dict()
+        errorMsgDict = indigo.Dict()
+        return (valuesDict, errorMsgDict)
+
+    def validateDeviceFactoryUi(self, valuesDict, devIdList):
+        self.logger.debug(u"validateDeviceFactoryUi: valuesDict = {}, devIdList = {}".format(valuesDict, devIdList))
+        errorsDict = indigo.Dict()
+        return (True, valuesDict, errorsDict)
+
+    def closedDeviceFactoryUi(self, valuesDict, userCancelled, devIdList):
+        if userCancelled:
+            self.logger.debug(u"closedDeviceFactoryUi: user cancelled")
+            return
+
+        self.logger.threaddebug(u"closedDeviceFactoryUi: valuesDict =\n{}\ndevIdList =\n{}".format(valuesDict, devIdList))
+        
+        # Create the controller device
+        
+        fbID = valuesDict['address']
+        fbDevice = self.device_info[fbID]
+        props = { "SupportsBatteryLevel": True,  "SupportsStatusRequest": True}
+        newdev = indigo.device.create(indigo.kProtocol.Plugin, address=fbID, name=fbDevice['title'], props=props, deviceTypeId="fireboard")
+        newdev.model = "FireBoard"
+        newdev.subModel = fbDevice['model']
+        newdev.replaceOnServer()
+        
+        # Now create the probe channels
+        
+        props = { "SupportsOnState": False, "SupportsSensorValue": True, "SupportsStatusRequest": False}
+
+        for channel in fbDevice['channels']:
+            newdev = indigo.device.create(indigo.kProtocol.Plugin, address=channel['id'], name=channel['channel_label'], props=props, deviceTypeId="fireboardChannel")
+            newdev.model = "FireBoard"
+            newdev.subModel = "Channel"
+            newdev.replaceOnServer()
+        
+        return
 
     ########################################
 
@@ -251,14 +308,15 @@ class Plugin(indigo.PluginBase):
             self.logger.error(u"getDevices: RequestException: " + str(err))
             return False
         
-        self.device_info = response.json()
-        self.logger.threaddebug(u"getDevices device_info = {}".format(self.device_info))
+        for fb in response.json():
+            self.device_info[fb['hardware_id']] = fb
+            
         self.logger.debug(u"getDevices: {} Devices".format(len(self.device_info)))
+        self.logger.threaddebug(u"getDevices device_info = {}".format(self.device_info))
 
-        for fbDevice in self.device_info:
+        for fbID, fbDevice in self.device_info.iteritems():
 
             fbName = fbDevice['title']
-            fbID = fbDevice['hardware_id']
             self.logger.debug(u"getDevices: name = {}, hardware_id = {}".format(fbName, fbID))
             
             if not fbID in self.knownDevices:
@@ -273,22 +331,40 @@ class Plugin(indigo.PluginBase):
                     dev.updateStateOnServer('batteryLevel', battery, uiValue='{}%'.format(battery))
 
                 states_list = []
-                states_list.append({'key': 'uuid', 'value': fbDevice['uuid']})
-                states_list.append({'key': 'title', 'value': fbDevice['title']})
-                states_list.append({'key': 'model', 'value': fbDevice['model']})
-                states_list.append({'key': 'created', 'value': fbDevice['created']})
+                states_list.append({'key': 'uuid',          'value': fbDevice['uuid']})
+                states_list.append({'key': 'title',         'value': fbDevice['title']})
+                states_list.append({'key': 'model',         'value': fbDevice['model']})
+                states_list.append({'key': 'created',       'value': fbDevice['created']})
                 states_list.append({'key': 'channel_count', 'value': fbDevice['channel_count']})
-                states_list.append({'key': 'degreetype', 'value': fbDevice['degreetype']})
-                states_list.append({'key': 'last_templog', 'value': fbDevice['last_templog']})
-                states_list.append({'key': 'probe_config', 'value': fbDevice['probe_config']})
-                states_list.append({'key': 'id', 'value': fbDevice['id']})
-                states_list.append({'key': 'hardware_id', 'value': fbDevice['hardware_id']})
-                states_list.append({'key': 'version', 'value': fbDevice['version']})
-                states_list.append({'key': 'fbj_version', 'value': fbDevice['fbj_version']})
-                states_list.append({'key': 'fbn_version', 'value': fbDevice['fbn_version']})
-                states_list.append({'key': 'fbu_version', 'value': fbDevice['fbu_version']})                    
+                states_list.append({'key': 'degreetype',    'value': fbDevice['degreetype']})
+                states_list.append({'key': 'last_templog',  'value': fbDevice['last_templog']})
+                states_list.append({'key': 'probe_config',  'value': fbDevice['probe_config']})
+                states_list.append({'key': 'id',            'value': fbDevice['id']})
+                states_list.append({'key': 'hardware_id',   'value': fbDevice['hardware_id']})
+                states_list.append({'key': 'version',       'value': fbDevice['version']})
+                states_list.append({'key': 'fbj_version',   'value': fbDevice['fbj_version']})
+                states_list.append({'key': 'fbn_version',   'value': fbDevice['fbn_version']})
+                states_list.append({'key': 'fbu_version',   'value': fbDevice['fbu_version']})                    
                 dev.updateStatesOnServer(states_list)
             
+                for channel in fbDevice['channels']:
+                    self.logger.threaddebug(u"getDevices: channel = {}".format(channel))
+                    channelID = str(channel['id'])
+                    if channelID in self.fbChannels:
+                        dev = indigo.devices[self.fbChannels[channelID]]
+                        states_list = []
+                        temp = channel.get('current_temp', None)
+                        if not temp:
+                            temp = 0.0
+                        states_list.append({'key': 'sensorValue',   'value': temp, 'uiValue': "{:1}°".format(temp)})
+                        states_list.append({'key': 'id',            'value': channel['id']})
+                        states_list.append({'key': 'channel',       'value': channel['channel']})
+                        states_list.append({'key': 'channel_label', 'value': channel['channel_label']})
+                        states_list.append({'key': 'created',       'value': channel['created']})
+                        states_list.append({'key': 'enabled',       'value': channel['enabled']})
+                        dev.updateStatesOnServer(states_list)
+                    
+                    
                 self.triggerCheck(dev)
 
     ########################################
